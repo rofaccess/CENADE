e2495d3c54f2dce282967b9534816c1b48890539
Rodrigo <rofaccess@gmail.com>
2016-03-07 21:44:08 -0300

    Se agrego el submenu para historial clinico en el layout principal #6

--- code/app/controllers/home_controller.rb
+++ code/app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
-	
+	before_action :set_submenu, only: [:index]
+
+	def set_submenu
+    	@submenu_layout = 'layouts/historial_clinico_submenu'
+  	end
+
 	def index
 	end
 end
