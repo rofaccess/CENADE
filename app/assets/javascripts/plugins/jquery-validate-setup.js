5f028456dab295d01368bbdbe12efd5fb6739d72
Rodrigo <rofaccess@gmail.com>
2016-03-08 03:41:36 -0300

    Los mensajes de jquery-validation ahora están en español, la contraseña acepta una longitud minima de 8 y maxima de la y debe contener una combinación de numeros y letras (mayusculas y minusculas) #5

---
+++ app/assets/javascripts/plugins/jquery-validate-setup.js
@@ -0,0 +1,46 @@
+// Configuraciones generales para las validaciones
+jQuery.validator.setDefaults({
+	errorElement:	'span',
+	errorClass:		'text-danger',
+	onfocusout: function(element) { $(element).valid(); },
+	highlight: function(element){
+		$(element).closest('.form-group').addClass('has-warning');
+	},
+	success: function(element){
+		$(element).closest('.form-group').removeClass('has-warning');
+	}
+});
+
+
+// Reglas para las validaciones
+$.validator.addClassRules({
+	required: {
+		required: true
+	},
+	date: {
+		dateITA: true
+	},
+	shortString: {
+		maxlength: 50
+	},
+	mediumString: {
+		maxlength: 150
+	},
+	longString: {
+		maxlength: 250
+	},
+	
+	passwordLength: {
+		maxlength: 20,
+		minlength: 8
+	},
+
+	passwordCheck: {
+		passwordCheck: true
+	}
+});
+
+
+$.validator.addMethod("passwordCheck",function(value,element){
+                return this.optional(element) || /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/.test(value);
+            },"La contraseña debe contener una combinación de números y letras (minúsculas y mayúsculas).");
