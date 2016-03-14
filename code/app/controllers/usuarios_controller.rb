5ca23276583f4cd7b1c0a53a58048a4161d74f14
KariSanabria <karina.sanabria75@gmail.com>
2016-03-14 02:05:10 +0100

    #7 El boton agregar despliega un modal

---
+++ code/app/controllers/usuarios_controller.rb
@@ -0,0 +1,17 @@
+class UsuariosController < ApplicationController
+	
+	before_action :set_submenu, only: [:index]
+
+	def set_submenu
+		@submenu_layout = 'layouts/submenu_configuracion'
+	end
+
+	def index
+		@usuarios= User.all
+	end
+
+	def new
+		@usuario = User.new
+
+    end
+end
