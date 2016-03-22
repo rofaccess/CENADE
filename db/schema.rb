0d1764725f7f00c42292fe3a0da566c41a9a26ee
lesliie <leslyee.05@gmail.com>
2016-03-22 03:04:33 -0300

    cambie de gema a carrierwave, y arregle la interfaz [issue #14]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321053206) do
+ActiveRecord::Schema.define(version: 20160320212208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,10 +58,6 @@ ActiveRecord::Schema.define(version: 20160321053206) do
     t.string   "empresa_logo"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
-    t.string   "imagen_file_name"
-    t.string   "imagen_content_type"
-    t.integer  "imagen_file_size"
-    t.datetime "imagen_updated_at"
   end
 
   create_table "empleados", force: :cascade do |t|
