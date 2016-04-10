0e10794753e8bb69215bdc8c5dc760a3fc3e2419
Rodrigo <rofaccess@gmail.com>
2016-04-10 11:13:38 -0400

    Issue #46 Agruegué un overlay sobre la lista de empleados, para un efecto de cargando, Tiempo Utilizado: 00h30m

--- code/app/assets/javascripts/application.js
+++ code/app/assets/javascripts/application.js
@@ -20,7 +20,7 @@
 //= require plugins/jquery-validate-setup.js
 //= require plugins/jquery-inputmask.js
 //= require plugins/jquery-inputmask-regex-extensions.js
-
+//= require plugins/loading-overlay-min.js
 
 //= require languages/jquery-validate-messages-es.js
 //= require languages/jquery-ui-datepicker-es.js
@@ -35,14 +35,18 @@ APP = {
 
     /* Para que el listado se actualice a medida que se tipea */
     initBuscador: function(){
-    	$('.buscador').on('keyup change', 'input', function () {
+    	$('.buscador').on('keyup', 'input', function () {
 			delay(function(){
-				$("#list").append('<div class="overlay"><i class="fa fa-refresh fa-spin"></i></div>');
-					
-				delay(function(){}, 500);
+				$("#list").LoadingOverlay("show", {
+    				image       : "",
+    				fontawesome : "fa fa-refresh fa-spin"
+				});				
 				
+				setTimeout(function(){},5000); //Quitar esto en producción
+
 				$('.buscador').submit();
 			}, 500);
+			$("#list").LoadingOverlay("hide", true);
 		});
     },
 
