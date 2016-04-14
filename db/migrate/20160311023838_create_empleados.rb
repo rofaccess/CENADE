12ddb2054c1f633da3e1aac41e740b16f3a6d3f3
Rodrigo <rofaccess@gmail.com>
2016-04-13 20:08:42 -0400

    Fix Migrations

--- db/migrate/20160311023838_create_empleados.rb
+++ db/migrate/20160311023838_create_empleados.rb
@@ -4,10 +4,15 @@ class CreateEmpleados < ActiveRecord::Migration
     
     t.integer  :persona_id, null: false    
    
-    t.integer  :especialidad_id
+    t.integer  :area_id
 
 	t.string   :type ,default: '' ,limit: Domain::TIPO_EMPLEADO, null: false
     t.string   :cargo ,default: '' ,limit: Domain::CARGO_EMPLEADO, null: false
+
+    t.string   :abr_profesion ,default: '' ,limit: Domain::ABREVIATURA_PROFESION, null: true  
+
+
+    t.decimal :costo  ,default: 0
 
     t.datetime :deleted_at
     t.timestamps
@@ -17,7 +22,7 @@ class CreateEmpleados < ActiveRecord::Migration
     add_index :empleados, :deleted_at,unique: true 
         
     add_foreign_key(:empleados, :personas, column: 'persona_id', on_delete: :restrict)   
-    add_foreign_key(:empleados, :especialidades, column: 'especialidad_id', on_delete: :restrict)
+    add_foreign_key(:empleados, :areas, column: 'area_id', on_delete: :restrict)
 
     # Se hacen algunas modificaciones a la tabla users
     add_foreign_key(:users, :empleados, column: 'empleado_id',on_delete: :restrict)
--- db/migrate/20160411203330_create_areas.rb
+++ db/migrate/20160411203330_create_areas.rb
@@ -1,10 +1,14 @@
 class CreateAreas < ActiveRecord::Migration
   def change
     create_table :areas do |t|
-      t.string :nombre, null: false
+      t.string  :nombre,default: '' ,limit: Domain::AREA ,null: false
+      t.decimal :costo_usual   ,default: 0
+      t.decimal :costo_casual  ,default: 0
       t.integer :costo
 
       t.timestamps null: false
     end
+
+    add_index :areas, :nombre, unique: true
   end
 end
