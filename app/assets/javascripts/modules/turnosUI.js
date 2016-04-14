e1fc6a1ae6a3fac7b619b1eeef34799767a2085d
lesliie <leslyee.05@gmail.com>
2016-04-13 23:56:57 -0400

    Arregle las vistas y código ajax, falta terminar (issue #45) tiempo: 04h:00m

--- app/assets/javascripts/modules/turnosUI.js
+++ app/assets/javascripts/modules/turnosUI.js
@@ -1,27 +1,13 @@
 var turnosUI = (function(){
 	return {	
-		cargarTurno: function(url, id, replace){
-			$.ajax({
-				url: url,
-				type: 'post',
-				dataType: 'html',
-				data: {id: id},
-				success: function(response){
-					$(replace).html(response);
-				}
-			});
-		},	
-				
+		
 
 		// Inicia el script en el formulario
-		initScript: function(){
+		initScript: function(addpacienteUrl){
+			//turnosUI.getPaciente(addpacienteUrl)
 			$("#nuevo-turno").last().validate();
 
 		}
 	};
 }());
 
-
-$(document).on("page:change", function(){
-	turnosUI.init();
-})
