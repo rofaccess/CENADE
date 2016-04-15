e1e0d28f48bf76583846d56de653d7a06ead2ad6
Rodrigo <rofaccess@gmail.com>
2016-04-15 14:47:22 -0400

    Agregado el bin wkhtmltopdf para que funcione en heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,8 @@
-WickedPdf.config ||= {}
-WickedPdf.config.merge!({
-  # your extra configurations here
-})
+WickedPdf.config = {
+  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+  #:layout => "pdf.html",
+  #:exe_path => '/usr/local/bin/wkhtmltopdf'
+  :exe_path => "#{Rails.root}/bin/wkhtmltopdf"
+  #:exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s,
+  #Dir.entries "#{Rails.root}/bin" 
+}
