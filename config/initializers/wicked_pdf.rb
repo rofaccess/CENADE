e4f96b902a0ad7fcc46980c514de51b98a3f1afa
Rodrigo <rofaccess@gmail.com>
2016-04-15 11:10:19 -0400

    Change wicked_pdf config for heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,4 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  #:exe_path => '/usr/local/bin/wkhtmltopdf'
-}
+WickedPdf.config ||= {}
+WickedPdf.config.merge!({
+  # your extra configurations here
+})
