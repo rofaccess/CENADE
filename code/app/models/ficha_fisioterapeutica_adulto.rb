f4995f1737e48dca6109e4fbb81a6da2c7d845f6
Rodrigo <rofaccess@gmail.com>
2016-05-04 15:48:09 -0400

    Issue #54 Desactive seleccionar paciente en edit y escondi el botón agregar paciente, hice que el show se vea igual al resto de las fichas, Tiempo Utilizado: 02h00m

--- code/app/models/ficha_fisioterapeutica_adulto.rb
+++ code/app/models/ficha_fisioterapeutica_adulto.rb
@@ -14,10 +14,11 @@ class FichaFisioterapeuticaAdulto < ActiveRecord::Base
 		self.area_id= area
 	end
 
-	# Estos alias fallan despues de guardar una ficha por eso dejo de usarse en el index
+	# //- Estos alias fallan despues de guardar una ficha por eso dejó de usarse en el index
 	ransack_alias :paciente, :paciente_persona_nombre_or_paciente_persona_apellido_or_paciente_persona_ci	
 	ransack_alias :doctor,   :doctor_persona_nombre_or_doctor_persona_apellido
-    #No se usa
+    
+    # //- No se usa todavía
 	#def validate_paciente 
 	#	paciente= FichaFisioterapeuticaAdulto.where("paciente_id = ?", self.paciente_id)
 	#	if !paciente.empty?
@@ -25,4 +26,7 @@ class FichaFisioterapeuticaAdulto < ActiveRecord::Base
 	#	end
 	#end
 
+	# Law of Demeter 
+	delegate :persona_nombre, :persona_apellido, :persona_full_name, to: :paciente, prefix: true, allow_nil: true
+	delegate :persona_nombre, :persona_apellido, :persona_full_name, :abr_profesion, to: :doctor, prefix: true, allow_nil: true
 end
