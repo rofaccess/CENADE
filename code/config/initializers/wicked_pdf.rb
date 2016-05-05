dd1b5c84699cef16ce4b068d84c1073e941a6df2
Rodrigo <rofaccess@gmail.com>
2016-05-05 00:41:39 -0400

    Arreglado problema de impresión

--- code/config/initializers/wicked_pdf.rb
+++ code/config/initializers/wicked_pdf.rb
@@ -2,5 +2,5 @@ WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
   #:exe_path => '/usr/local/bin/wkhtmltopdf'
-   :exe_path =>'C:\Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
+   :exe_path => "#{Rails.root}/bin/wkhtmltopdf"
 }
