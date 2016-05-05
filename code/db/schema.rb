e9c2e221c46ebe15d99c5eb2dbb7cd5e6e91608b
KariSanabria <karina.sanabria75@gmail.com>
2016-05-04 21:30:46 -0400

    Issue #63 Cambios en el migration Tiempo Utilizado:1h:30m

--- code/db/schema.rb
+++ code/db/schema.rb
@@ -287,45 +287,16 @@ ActiveRecord::Schema.define(version: 20160504025744) do
   end
 
   create_table "fichas_odontologicas", force: :cascade do |t|
-    t.integer  "paciente_id",               null: false
-    t.integer  "area_id",                   null: false
-    t.integer  "doctor_id",                 null: false
+    t.integer  "paciente_id",                             null: false
+    t.integer  "area_id",                                 null: false
+    t.integer  "doctor_id",                               null: false
     t.integer  "nro_ficha"
-    t.date     "fecha"
-    t.string   "motivo_consulta"
-    t.string   "observaciones"
-    t.string   "servicio_cenade"
-    t.string   "medicacion_actual"
-    t.string   "anestesico"
-    t.string   "penicilina"
-    t.string   "otros_medicamentos"
-    t.string   "hemorragias"
-    t.string   "problema_tratamiento"
-    t.string   "enfermedad_cardiovascular"
-    t.string   "diabetes"
-    t.string   "hepatitis"
-    t.string   "enfermedades_renales"
-    t.string   "artritis"
-    t.string   "tuberculosis"
-    t.string   "enfermedades_venereas"
-    t.string   "enfermedades_sanguineas"
-    t.string   "fumador"
-    t.string   "enfermedades_neurologicas"
-    t.string   "menstruacion"
-    t.string   "embarazada"
-    t.string   "tiene_hijos"
-    t.string   "cantidad_hijos"
-    t.string   "amamanta"
-    t.string   "hospitalizado"
-    t.string   "causa_hospitalizado"
-    t.string   "ecg"
-    t.string   "tac"
-    t.string   "rx"
-    t.string   "laboratorios"
-    t.string   "otros_examenes"
-    t.string   "tratamiento"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.date     "fecha",                                   null: false
+    t.string   "nombre_tutor",    limit: 30, default: ""
+    t.string   "profesion_tutor", limit: 50, default: ""
+    t.string   "tel_tutor",       limit: 50, default: ""
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
   end
 
   create_table "fichas_psicopedagogicas", force: :cascade do |t|
