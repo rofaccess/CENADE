4b5f585e10ca712d623d4f342670b81ada615f06
lesliie <leslyee.05@gmail.com>
2016-05-09 01:21:36 -0400

    fecha 08/05: "Agregué consultas en forma de tabs al show a  ficha Fisio Niño"(issue #66) tiempo 05h:00m

--- code/app/models/ficha_fisioterapia_nino.rb
+++ code/app/models/ficha_fisioterapia_nino.rb
@@ -1,31 +1,20 @@
 class FichaFisioterapiaNino < ActiveRecord::Base
 	paginates_per 20
 
+	#incremento automatico de nro de ficha
+	protokoll :nro_ficha, pattern: '#'
+
 	#asociaciones
 	belongs_to :paciente
 	belongs_to :area
 	belongs_to :doctor, :foreign_key => :doctor_id
 
 	#validaciones
-	validates :condicion_general , length: { maximum: 700, message: ' soporta un máximo 700 caracteres' }
+	validates :condicion_general , length: { maximum: 500, message: ' soporta un máximo 500 caracteres' }
 
 	#cargas automáticas
 	before_create :cargar_area_id
-	before_create :actualizar_nro
 
-
-
-	def actualizar_nro
-      ficha = FichaFisioterapiaNino.last
-      if ficha.nil? 
-        self.nro_ficha = 1
-      elsif  ficha.nro_ficha.nil?
-      	self.nro_ficha = 1
-      else
-        ficha_nro = ficha.nro_ficha
-        self.nro_ficha = ficha_nro+1
-      end
-    end
 
 	def cargar_area_id
 		area= Area.where(nombre: 'Fisioterapia').first.id
@@ -44,4 +33,7 @@ class FichaFisioterapiaNino < ActiveRecord::Base
 			 :persona_fecha_nacimiento,:persona_telefono, :persona_direccion,
 			 :fecha_ingreso, 
 			 to: :paciente, prefix: true, allow_nil: true
+
+	delegate :persona_nombre, :persona_apellido, :persona_full_name, :abr_profesion, to: :doctor, prefix: true, allow_nil: true
+
 end
