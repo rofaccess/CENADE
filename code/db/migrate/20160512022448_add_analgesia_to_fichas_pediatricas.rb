5b6786af59e1a78aaf149db04eb7a85567bb9e75
Rodrigo <rofaccess@gmail.com>
2016-05-11 22:39:07 -0400

    Remove migration AddAnalgesiaToFichasPediatricas

--- code/db/migrate/20160512022448_add_analgesia_to_fichas_pediatricas.rb
+++
@@ -1,5 +0,0 @@
-class AddAnalgesiaToFichasPediatricas < ActiveRecord::Migration
-  def change
-  	add_column :fichas_pediatricas, :analgesia, :string, null: true, default: '',limit: Domain::DESC50
-  end
-end
