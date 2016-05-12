01c334ffd3308b397e43e9e47658573c5dc70f5e
Rodrigo <rofaccess@gmail.com>
2016-05-11 21:07:26 -0400

    Issue #61 Agruegué los tabs de consulta al show de ficha pediátrica, Tiempo Utilizado: 00h30m

--- code/app/assets/javascripts/modules/consultasUI.js
+++ code/app/assets/javascripts/modules/consultasUI.js
@@ -1,107 +1,123 @@
 var consultasUI = (function(){
-	return {		
+	return {
 
-		init: function(){	
+		init: function(){
 			$('body').on('click', '.show-consulta', function(e){
 				$.get($(this).parents('tr').data('url'), {}, function(){}, 'script');
-			});			
-		},			
+			});
+		},
+
+    initConsultasTab: function(){
+      var tabs;
+      jQuery(function($) {
+        tabs = $('.tabscontent').tabbedContent({loop: true}).data('api');
+
+
+                // Next and prev actions
+                $('.controls a').on('click', function(e) {
+                  var action = $(this).attr('href').replace('#', '');
+                  tabs[action]();
+                  e.preventDefault();
+                });
+              });
+    },
+
 
     advancedSearchControl: function(){
-          $(".to-hide").hide();
+      $(".to-hide").hide();
 
-             $(document).ready(function(){
-              var show=true;
-              $("#show").click(function(){
-               if(show){
-                  $("#advanced-search").show();
-                  show=false;
-               }else{
-                  $("#advanced-search").hide();
-                  show=true;
-               }
+      $(document).ready(function(){
+        var show=true;
+        $("#show").click(function(){
+         if(show){
+          $("#advanced-search").show();
+          show=false;
+        }else{
+          $("#advanced-search").hide();
+          show=true;
+        }
 
-            });
-          });
-        },
-			
-		selectControl: function(){
-            $(".paciente_select").select2({
-                placeholder: "Seleccione un paciente",
-                language: "es",
-                theme: "bootstrap"
+      });
+      });
+    },
 
-               }).on("select2:select",function(){
-                $(this).valid();
-                id = $(this).val();
+    selectControl: function(){
+      $(".paciente_select").select2({
+        placeholder: "Seleccione un paciente",
+        language: "es",
+        theme: "bootstrap"
 
-                $.ajax({
-                 
-                  url: "/consultas/get_paciente",
-                  type: 'get',
-                  data: {
-                   id : $(this).val()
-                  },
-                  success: function(resp){
+      }).on("select2:select",function(){
+        $(this).valid();
+        id = $(this).val();
+
+        $.ajax({
+
+          url: "/consultas/get_paciente",
+          type: 'get',
+          data: {
+           id : $(this).val()
+         },
+         success: function(resp){
                       //alert("Data");
-                   }
-                  
-                 });
-              });
-              $(".profesional_select2").select2({
-                placeholder: "Seleccione un Profesional",
-                language: "es",
-                theme: "bootstrap"
+                    }
 
-                }).on('change', function () {
-                    $(this).valid();
+                  });
+      });
+      $(".profesional_select2").select2({
+        placeholder: "Seleccione un Profesional",
+        language: "es",
+        theme: "bootstrap"
 
-                });
-            $(".area_select").select2({
-                placeholder: "Seleccione un Área",
-                theme: "bootstrap",
-                language: "es"
+      }).on('change', function () {
+        $(this).valid();
 
-                }).on('change', function () {
-                    $(this).valid();
-                    id = $(this).val();
+      });
+      $(".area_select").select2({
+        placeholder: "Seleccione un Área",
+        theme: "bootstrap",
+        language: "es"
 
-                    $.ajax({
-                     
-                      url: "/consultas/recarga_profesional",
-                      type: 'get',
-                      data: {
-                       id : $(this).val()
-                      },
-                      success: function(resp){
+      }).on('change', function () {
+        $(this).valid();
+        id = $(this).val();
+
+        $.ajax({
+
+          url: "/consultas/recarga_profesional",
+          type: 'get',
+          data: {
+           id : $(this).val()
+         },
+         success: function(resp){
                           //alert("Data");
-                       }
-                      
-                     });
-                });
-          
-        },
-		
+                        }
+
+                      });
+      });
+
+    },
+
 		// Inicia el script en el formulario
 		initScript: function(){
 			consultasUI.selectControl();
-      consultasUI.advancedSearchControl();			
+      consultasUI.advancedSearchControl();
 
-			$('.datepicker').datepicker({
-		        format: "dd/mm/yyyy",
-		        language: "es",
-		        autoclose: true,
-		        orientation: "bottom",
-		        theme: "bootstrap"		       
-		        }).on('change', function() {
-        			$(this).valid();
-		    });
-			
+      $('.datepicker').datepicker({
+        format: "dd/mm/yyyy",
+        language: "es",
+        autoclose: true,
+        orientation: "bottom",
+        theme: "bootstrap"
+      }).on('change', function() {
+       $(this).valid();
+     });
+
 		   	//Valida el formulario antes de enviarlo
-		  	$('.nueva-consulta').last().validate();
-		}
-	};
-}());
+       $('.nueva-consulta').last().validate();
+     }
+   };
+ }());
 
 $(function(){
 	consultasUI.init();
