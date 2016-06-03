e767b3ce8e80d4a6bd6228e4f9014963cf90c16f
lesliie <leslyee.05@gmail.com>
2016-06-03 03:35:00 -0400

    fecha 02/07: "Arreglo de conflictos"(issue #70 y #65) tiempo 04hs

--- code/app/controllers/reportes_estadisticos_controller.rb
+++ code/app/controllers/reportes_estadisticos_controller.rb
@@ -18,17 +18,56 @@ class ReportesEstadisticosController < ApplicationController
     get_reportes
   end
 
+  def print_reporte_anuales
+      @reportes = ReporteEstadistico.select("reportes_estadisticos.area_id, reportes_estadisticos.doctor_id, SUM(cantidad) as cantidad_por_anho, SUM(cant_pend) as cantidad_por_aten, anho").group("reportes_estadisticos.area_id, reportes_estadisticos.doctor_id, anho")
+
+      respond_to do |format|
+        format.pdf do
+          render pdf:   'Reporte Anual',
+          template:     'reportes_estadisticos/print_reporte_anuales.pdf.erb',
+          layout:       'pdf.html',
+          orientation:  'Landscape',
+          title:        'Reporte Anual',
+          footer: {
+            center: '[page] de [topage]',
+            right: "#{Formatter.format_datetime(Time.now)}"
+          }
+          #disposition:  'attachment' # Ya descarga el pdf en la pc
+        end
+      end
+    end
+
+  def print_reporte_mensuales
+      @reportes = ReporteEstadistico.all
+
+      respond_to do |format|
+        format.pdf do
+          render pdf:   'Reporte Mensual',
+          template:     'reportes_estadisticos/print_reporte_mensuales.pdf.erb',
+          layout:       'pdf.html',
+          orientation:  'Landscape',
+          title:        'Reporte Mensual',
+          footer: {
+            center: '[page] de [topage]',
+            right: "#{Formatter.format_datetime(Time.now)}"
+          }
+          #disposition:  'attachment' # Ya descarga el pdf en la pc
+        end
+      end
+    end
+
   def buscar
     get_reportes_por_mes
     render 'por_mes'
   end
+
   def buscar_anho
     get_reportes
     render 'por_anho'
   end
 
   def get_reportes
-    @reporte = ReporteEstadistico.select("area_id, doctor_id, SUM(cantidad) as cantidad_por_anho, SUM(cant_pend) as cantidad_por_aten, anho").group("area_id, doctor_id, anho")
+    @reporte = ReporteEstadistico.select("reportes_estadisticos.area_id, reportes_estadisticos.doctor_id, SUM(cantidad) as cantidad_por_anho, SUM(cant_pend) as cantidad_por_aten, anho").group("reportes_estadisticos.area_id, reportes_estadisticos.doctor_id, anho")
     @search= @reporte.search(params[:q])
     @reportes= @search.result.page(params[:page])
   end
