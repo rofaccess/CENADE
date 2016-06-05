653da436f555ab2e1bdfd89e0a7136111b011dd8
Rodrigo <rofaccess@gmail.com>
2016-06-04 22:46:14 -0400

    Issue #77 Nuevo consulta adulto y pediátrico funcional en nutrición, Tiempo Utilizado: 01h00m 04/06

--- code/app/controllers/atenciones_profesionales_controller.rb
+++ code/app/controllers/atenciones_profesionales_controller.rb
@@ -19,9 +19,7 @@ class AtencionesProfesionalesController < ApplicationController
 
     case @turno.area_nombre
     when "Clínico"
-      get_data_clinico
     when "Fisioterapia"
-      get_data_fisioterapia
     when "Fonoaudiología"
     when "Neurología"
     when "Nutrición"
@@ -38,9 +36,9 @@ class AtencionesProfesionalesController < ApplicationController
     @consulta_ped = ConsultaNutricionalPediatrica.new(consulta_ped_params)
 
     if @consulta_ped.save
-      flash.now[:notice] = 'Control registrado exitosamente'
+      flash.now[:notice] = 'Consulta registrada exitosamente'
     else
-      flash.now[:alert] = "No se ha podido guardar el control."
+      flash.now[:alert] = "No se ha podido guardar la consulta."
     end
     render 'atenciones_profesionales/nutricion/create_consulta_ped', format: :js
   end
@@ -50,6 +48,29 @@ class AtencionesProfesionalesController < ApplicationController
        :sento, :paro,:camino, :sigue_luz, :rie_llora, :busca_sonido, :emite_sonido, :habilidades, :mastica_deglute, :otros, :desayuno, :media_manana, :almuerzo,
        :merienda, :cena, :cargo_quien, :diarrea, :vomitos, :fiebre, :constipacion, :orina, :sudor, :problemas_respiratorios,
        :distension_abdominal, :otros2, :diagnostico, :peso, :talla, :pc, :imc, :cm)
+  end
+
+  def create_consulta_ad
+    @consulta_ad = ConsultaNutricionalAdulto.new(consulta_ad_params)
+
+    if @consulta_ad.save
+      flash.now[:notice] = 'Consulta registrada exitosamente'
+    else
+      flash.now[:alert] = "No se ha podido guardar la consulta."
+    end
+    render 'atenciones_profesionales/nutricion/create_consulta_ad', format: :js
+  end
+
+  def consulta_ad_params
+    params.require(:consulta_nutricional_adulto).permit(:ficha_nutricional_adulto_id, :doctor_id, :fecha,
+      :motivo_consulta, :actuales, :dx, :peso_actual, :peso_ideal, :peso_deseable, :talla, :biotipo,
+      :cir_muneca, :circ_brazo, :circ_cintura, :imc, :evaluacion, :medicamentos, :suplementos, :apetito,
+      :factores_apetito, :alergia_intolerancia, :cae_cabello, :estado_bucal, :orina_bien, :ir_cuerpo,
+      :actividades_fisicas, :tipo, :hs_act_fisicas, :frecuencia, :actividad_laboral, :horas_laborales,
+      :vive_con, :quien_prepara, :que_elementos, :toma_agua, :mastica_deglute, :dificultad_beber, :hora_acuesta,
+      :hora_levanta, :duerme_bien, :habilidades, :tratamientos_cenade, :alim_desayuno, :alim_media, :alim_almuerzo,
+      :alim_merienda, :alim_cena, :cant_desayuno, :cant_media, :cant_almuerzo, :cant_merienda, :cant_cena,
+      :modo_desayuno, :modo_media, :modo_almuerzo, :modo_merienda, :modo_cena, :lugar_desayuno, :lugar_media, :lugar_almuerzo, :lugar_merienda, :lugar_cena, :indicaciones)
   end
 
   def create_control
